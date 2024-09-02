2690923562713bd5e53fa7262e0ad78c78884064
Gabriella Massaro <gmassaro807@gmail.com>
2024-09-01 21:07:23 -0400

    Create method to resize image as thumbnail, get vips installed locally

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   has_many :user_genres
   has_many :genres, through: :user_genres
   has_one_attached :image
+
+  def image_as_thumbnail
+    image.variant(resize_to_limit: [150, 150]).processed
+  end
 end
