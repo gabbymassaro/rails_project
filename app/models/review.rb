c5dc3119cf3bc125ccb835d60e22e0e6f2d7e3a7
Gabriella Massaro <gmassaro807@gmail.com>
2024-09-04 11:45:30 -0400

    Create average rating method and display on books index

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ApplicationRecord
   belongs_to :book
 
   validates :rating, :content, :user_id, :book_id, presence: true
+
+  def self.average_rating
+    average(:rating)
+  end
 end
