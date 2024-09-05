dc3ee96d7b11006c4ba157db321fb8d924b3f4da
Gabriella Massaro <gmassaro807@gmail.com>
2024-09-05 09:27:07 -0400

    Remove unused method on registration_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,14 +13,4 @@ protected
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
-
-  # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    super(resource)
-  end
-
-  # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    super(resource)
-  end
 end
