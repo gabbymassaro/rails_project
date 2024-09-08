35db07aa902e952d3ef3f71d2ce3915c356bb741
Gabriella Massaro <gmassaro807@gmail.com>
2024-09-08 18:13:28 -0400

    Fix search test

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -28,13 +28,19 @@ RSpec.describe 'Book' do
 
   describe '.search' do
     it 'returns the query results for a book by title' do
-      book = build(:book)
-      expect(book.title).to eq(book.title)
+      book = create(:book)
+
+      found = Book.search(book.title)
+
+      expect(found.first.title).to eq(book.title)
     end
 
     it 'returns the query results for a book by author' do
-      book = build(:book)
-      expect(book.author).to eq(book.author)
+      book = create(:book)
+
+      found = Book.search(book.author)
+
+      expect(found.first.author).to eq(book.author)
     end
   end
 
