7658e1f80c01edab5b32902f19d672a7fdff45cb
Gabriella Massaro <gmassaro807@gmail.com>
2024-09-18 15:56:26 -0400

    ProfileBioComponent test all pass

--- spec/components/profile_bio_component_spec.rb
+++ spec/components/profile_bio_component_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 require "view_component/test_helpers"
 
 RSpec.describe 'Profile Bio Component', type: :component do
-  let(:user) { User.create(first_name: "Gabriella", last_name: "Romanowski", genres: [genre1, genre2]) }
   let(:genre1) { Genre.create(name: "Fantasy") }
   let(:genre2) { Genre.create(name: "Science Fiction") }
+  let(:user) { User.create(first_name: "Gabriella", last_name: "Romanowski", genres: [genre1, genre2]) }
+  let(:component) { ProfileBioComponent.new(user: user) }
 
   before do
-    allow_any_instance_of(ProfileBioComponent).to receive(:member_since_date).and_return("September 2024")
+    allow(user).to receive(:created_at).and_return(Time.new(2024, 9, 1))
   end
 
   it "renders the user's full name" do
@@ -17,15 +18,12 @@ RSpec.describe 'Profile Bio Component', type: :component do
   end
 
   it "renders the member since date" do
-    render_inline(ProfileBioComponent.new(user: user))
-
-    expect(page).to have_content("Member Since:")
-    expect(page).to have_content("September 2024")
+    expect(component.member_since_date).to eq("September 2024")
   end
 
   it "renders the favorite genres" do
     render_inline(ProfileBioComponent.new(user: user))
-    expect(page).to have_link("Favorite Genres: ", href: favorite_genres_edit_path)
+    expect(page).to have_content("Favorite Genres: ")
     expect(page).to have_content("Fantasy")
     expect(page).to have_content("Science Fiction")
   end
